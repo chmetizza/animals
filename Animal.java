6d75e04d02d4275edd997640b32fab8e07a9ac83
chmetizza <sh.corridor@gmail.com>
2017-07-11 23:01:47 +0200

    Update Animal.java

--- Animal.java
+++ Animal.java
@@ -1,2 +1,35 @@
 # animals
-test java
+
+package si.rais;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * Created by spela on 11. 07. 2017.
+ */
+public class Animal {
+    private int age;
+    private LocalDate birthDay;
+
+    public Animal(int age, LocalDate birthDay) {
+        this.age = age;
+        this.birthDay = birthDay;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+}
